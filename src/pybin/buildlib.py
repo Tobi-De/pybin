74e8bd04ed3182decb6f822e81351ab31e4c925c
Tobi DEGNON <tobidegnon@proton.me>
2024-10-21 08:37:02 +0100

    change distribution name

--- src/pybin/buildlib.py
+++ src/pybin/buildlib.py
@@ -39,8 +39,8 @@ def convert_archive_to_wheel(
         license: str,
         compression_mode: Optional[str],
 ):
-    distribution_name = f'{name}_bin'  # If wheel names have a hyphen, the RECORD file is placed in the wrong .dist-info directory resulting in invalid wheels.
-    pypi_distribution_name = f'{name}-bin'
+    distribution_name = f'{name}_bin_edge'  # If wheel names have a hyphen, the RECORD file is placed in the wrong .dist-info directory resulting in invalid wheels.
+    pypi_distribution_name = f'{name}-bin-edge'
     contents = {}
 
     # Extract the command binary
