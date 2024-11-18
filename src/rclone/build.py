9a21f0bcffdd7950f21683acba7b3791ab8d657f
Tobi-De <40334729+Tobi-De@users.noreply.github.com>
2024-11-18 01:03:56 +0000

    Update packages version to match upstream

--- src/rclone/build.py
+++ src/rclone/build.py
@@ -2,8 +2,8 @@ from ..buildlib import build_wheels
 
 NAME = 'rclone'
 UPSTREAM_REPO = "https://github.com/rclone/rclone"
-VERSION = '1.68.1'
-PYPI_VERSION = '1.68.1'
+VERSION = '1.68.2'
+PYPI_VERSION = '1.68.2'
 SUMMARY = f"A thin wrapper to distribute {UPSTREAM_REPO} via pip."
 LICENSE = "MIT"
 
